e7fab21f4dfad7880d83775615203aad788bb573
s33chin <shane.chin97@yahoo.com>
2023-06-07 22:10:41 -0500

    started register page

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'), # type: ignore
     #path('login/', views.login_user, name='login'), # type: ignore
     path('logout/', views.logout_user, name='logout'), # type: ignore
+    path('register/', views.register_user, name='register'),  # type: ignore
 
 ]
